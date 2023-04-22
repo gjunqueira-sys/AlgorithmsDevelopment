e793fe301718815400cff02ff22ef218c82b7394
Documenter.jl <documenter@juliadocs.github.io>
2023-04-22 13:23:41 +0000

    build based on c4a98a5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AlgorithmsDevelopment","category":"page"},{"location":"#AlgorithmsDevelopment","page":"Home","title":"AlgorithmsDevelopment","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AlgorithmsDevelopment.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AlgorithmsDevelopment]","category":"page"}]
+}
